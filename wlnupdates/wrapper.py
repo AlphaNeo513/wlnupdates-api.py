b97b0e97a26b69ce2429713c1396d91fcae125bb
AlphaNeo513 <38252734+AlphaNeo513@users.noreply.github.com>
2020-08-31 15:21:55 -0400

    Added a lot more features
    Since the get is very similar, I just used copy and paste. I've added in get_watches, get_publisher_data, get_group_data, get_artist_data, get_author_data, and get_genre_data

--- wlnupdates/wrapper.py
+++ wlnupdates/wrapper.py
@@ -13,14 +13,96 @@ class Request:
 
 
     def get_series_data(self, id):
-            payload = '{"id":'+ id +', "mode": "get"}'
-            response = requests.request("POST", self.url, headers=self.headers, data=payload)
+        payload = '{"id":'+ id +', "mode": "get-series-id"}'
+        response = requests.request("POST", self.url, headers=self.headers, data=payload)
+        json_data = response.json()
+        if json_data['error'] is True:
+            message = 'Error! '+json_data['message']
+            return message
+        else:
+            return json_data
+
+    def get_watches(self, id):
+        payload = '{"id":'+ id +', "mode": "get-tag-id"}'
+        response = requests.request("POST", self.url, headers=self.headers, data=payload)
+        try:
             json_data = response.json()
-            if json_data['error'] is True:
-                print('Error! '+json_data['message'])
-            else:
-                return json_data
+        except:
+            return f"ID: {id} does not exist"
+        if json_data['error'] is True:
+            message = 'Error! '+json_data['message']
+            return message
+        else:
+            return json_data
+
+    def get_publisher_data(self, id):
+        payload = '{"id":'+ id +', "mode": "get-publisher-id"}'
+        response = requests.request("POST", self.url, headers=self.headers, data=payload)
+        try:
+            json_data = response.json()
+        except:
+            return f"ID: {id} does not exist"
+        if json_data['error'] is True:
+            message = 'Error! '+json_data['message']
+            return message
+        else:
+            return json_data
+
+    def get_group_data(self, id):
+        payload = '{"id":'+ id +', "mode": "get-group-id"}'
+        response = requests.request("POST", self.url, headers=self.headers, data=payload)
+        try:
+            json_data = response.json()
+        except:
+            return f"ID: {id} does not exist"
+        if json_data['error'] is True:
+            message = 'Error! '+json_data['message']
+            return message
+        else:
+            return json_data
+
+    def get_artist_data(self, id):
+        payload = '{"id":'+ id +', "mode": "get-artist-id"}'
+        response = requests.request("POST", self.url, headers=self.headers, data=payload)
+        try:
+            json_data = response.json()
+        except:
+            return f"ID: {id} does not exist"
+        if json_data['error'] is True:
+            message = 'Error! '+json_data['message']
+            return message
+        else:
+            return json_data
+
+    def get_author_data(self, id):
+        payload = '{"id":'+ id +', "mode": "get-author-id"}'
+        response = requests.request("POST", self.url, headers=self.headers, data=payload)
+        try:
+            json_data = response.json()
+        except:
+            return f"ID: {id} does not exist"
+        if json_data['error'] is True:
+            message = 'Error! '+json_data['message']
+            return message
+        else:
+            return json_data
+
+    def get_genre_data(self, id):
+        payload = '{"id":'+ id +', "mode": "get-genre-id"}'
+        response = requests.request("POST", self.url, headers=self.headers, data=payload)
+        try:
+            json_data = response.json()
+        except:
+            return f"ID: {id} does not exist"
+        if json_data['error'] is True:
+            message = 'Error! '+json_data['message']
+            return message
+        else:
+            return json_data
+
+
+
 data = Request()
 
 
-pprint(data.get_series_data('3'))
+pprint(data.get_group_data('01'))
