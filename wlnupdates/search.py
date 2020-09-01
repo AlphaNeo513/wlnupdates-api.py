3acafd5ac4f31095d97f81982d03f3f22348bf3f
AlphaNeo513 <38252734+AlphaNeo513@users.noreply.github.com>
2020-09-01 16:11:56 -0400

    Initial commit for search_advanced
    Initial commit for the function search_advanced.

--- wlnupdates/search.py
+++ wlnupdates/search.py
@@ -1,5 +1,7 @@
 #Python file for searching novels by string and exporting ID
 import requests
+import sys
+import json
 from pprint import pprint
 class Search:
     def __init__(self):
@@ -23,7 +25,35 @@ class Search:
             else:
                 pprint(json_data)
 
+    def search_advanced(self, name=None, tag_category=None, genre_category=None, chapter_limits=None, series_type=None, sort_mode=None):
+        payload = {"mode": "search-advanced"}
+        if name:
+            payload['title-search-text'] = name
+        elif tag_category:
+            payload['tag-category'] = tag_category
+        elif genre_category:
+            payload['genre-category'] = genre_category
+        elif chapter_limits:
+            payload['chapter-limits'] = chapter_limits
+        elif series_type:
+            payload['series-type'] = series_type
+        elif sort_mode:
+            payload['sort-mode'] = sort_mode
+        payload = json.dumps(payload)
+        print(str(payload))
+
+
+
+
+        test = { 'mode' : 'search-advanced', 'series-type' : {'Translated' : 'included'}, 'tag-category' : { 'ability-steal' : 'included', 'virtual-reality' : 'excluded' }, 'sort-mode' : "update", 'chapter-limits' : [40, 0], }
+
+        
+
+
+        response = requests.request("POST", self.url, headers=self.headers, data="'"+json.dumps(test)+"'")
+        return response
+    
 
 test = Search()
 
-print(test.title_search('The Book Eating Magician', similarityNum=0, fullList=True))
+print(test.search_advanced(series_type={'Translated' : 'included'}, tag_category={ 'ability-steal' : 'included', 'virtual-reality' : 'excluded' }))
