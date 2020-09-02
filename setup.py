f0ba7293a3b141d38b268e2d7420f4df7a5df9f1
AlphaNeo513 <38252734+AlphaNeo513@users.noreply.github.com>
2020-09-02 13:35:25 -0400

    inital commit
    initial commit for PyPi project

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+from wlnupdates import __title__, __author__, __version__
+
+if not __title__:
+    raise RuntimeError('title is not set')
+if not __author__:
+    raise RuntimeError('Author is not set')
+if not __version__:
+    raise RuntimeError('Version is not set')
+
+with open("README.md", "r") as f:
+    readme = f.read()
+
+
+setup(
+    name=__title__,
+    version=__version__,
+    url = 'https://github.com/AlphaNeo513/wlnupdates-api.py',
+    python_requires=">= 3.7",
+    description='A simple API wrapper for wlnupdates',
+    long_description=readme,
+    author='AlphaNeo',
+    author_email='sewon360@gmail.com',
+    license='MIT',
+     
+)
---
+++ wlnupdates/__init__.py
@@ -0,0 +1,6 @@
+__title__ = 'wlnupdates'
+__author__ = 'AlphaNeo'
+__license__ = 'MIT'
+__version__ = '1.0.0'
+from .search import Search
+from .wrapper import Wrapper
