6ae79f865e5f52c68cbd3e164712b652d41d0089
AlphaNeo513 <38252734+AlphaNeo513@users.noreply.github.com>
2020-09-02 13:54:07 -0400

    Update to 1.0.1

--- setup.py
+++ setup.py
@@ -1,20 +1,12 @@
 from setuptools import setup, find_packages
-from wlnupdates import __title__, __author__, __version__
-
-if not __title__:
-    raise RuntimeError('title is not set')
-if not __author__:
-    raise RuntimeError('Author is not set')
-if not __version__:
-    raise RuntimeError('Version is not set')
 
 with open("README.md", "r") as f:
     readme = f.read()
 
 
 setup(
-    name=__title__,
-    version=__version__,
+    name='wlnupdates',
+    version='1.0.1',
     url = 'https://github.com/AlphaNeo513/wlnupdates-api.py',
     python_requires=">= 3.7",
     description='A simple API wrapper for wlnupdates',
